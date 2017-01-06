e4d88098380136a9e55d441600f3a6cfa6bf61b9
jaymessina3 <jaymessina3@gmail.com>
2017-01-06 17:53:51 -0500

    4th Meal App Intro

---
+++ 4thMealApp/4thMealApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  4thMealApp
+//
+//  Created by Jay Messina on 1/6/17.
+//  Copyright © 2017 Jay Messina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    print("Hi Puck")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
